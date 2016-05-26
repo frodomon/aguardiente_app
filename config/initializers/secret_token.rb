caa88c812036b951a80514add1e8044bf5612ab5
Alfredo Vasquez <vasquez.alf@gmail.com>
2016-05-26 18:02:01 -0500

    Aplicación creada

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AguardienteApp::Application.config.secret_key_base = '1eca2b18a0185b9bf7f88134aa4b99bd2049b9b023eb9f9444e2cb4b373748c4b7e9939450bfdc100cba0b12965b77837245f961d8e302da8b4bb81375bd3790'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AguardienteApp::Application.config.session_store :cookie_store, key: '_aguardiente_app_session'
